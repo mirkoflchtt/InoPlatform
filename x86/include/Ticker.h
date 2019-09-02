f56fe50a33c74b12b66e2a628490f9003003676a
mirkoflchtt <mirko.monmarche@gmail.com>
2019-09-02 08:03:55 +0200

    Complete emulation of Ticker class

--- x86/include/Ticker.h
+++ x86/include/Ticker.h
@@ -3,30 +3,56 @@
 
 #include "Arduino.h"
 
-typedef void (*tickerCB)(void);
+typedef void(*   callback_t) (void);
 
 #ifdef __cplusplus
 
 class Ticker {
 public:
   Ticker(void) :
-    interval(0), ts(0), cb(NULL)
+    rearm(false), interval(0), ts(0), cb(NULL)
   {}
 
   void trigger(const uint32_t _now)
   {
-    if (cb && (_now+interval)>=ts) { cb(); ts = _now; }
+    if (cb && (interval>0) && (_now+interval)>=ts) {
+      cb();
+      ts = _now;
+      if ( !rearm ) detach();
+    }
   }
 
-  void attach(const uint32_t _interval, tickerCB _cb)
-  {
-    interval = _interval; cb = _cb;
+  void  attach(float seconds, callback_t callback) {
+    attach_ms((uint32_t)(seconds * 1000 + 0.5f), callback);
   }
-  
+ 
+  void  attach_ms(uint32_t milliseconds, callback_t callback)
+  {
+    rearm    = true;
+    ts       = millis();
+    interval = milliseconds;
+    cb       = callback;
+  }
+
+  void  once(float seconds, callback_t callback) {
+    once_ms((uint32_t)(seconds * 1000 + 0.5f), callback);
+  }
+ 
+  void  once_ms(uint32_t milliseconds, callback_t callback)
+  {
+    attach_ms(milliseconds, callback);
+    rearm    = false;
+  }
+
+  void detach(void) {
+    interval = 0;
+  }
+
 private:
+  bool       rearm;
   uint32_t   interval;
   uint32_t   ts;
-  tickerCB   cb;
+  callback_t cb;
 };
 
 extern "C" {
