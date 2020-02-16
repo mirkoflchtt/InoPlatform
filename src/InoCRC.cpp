5d228773c9b6fc6510d4e93c5a15fd344e31d9e5
mirkoflchtt <mirko.monmarche@gmail.com>
2020-02-16 17:51:31 +0100

    Bump to library version 1.4.5, add 2 more crc checksums
      - Add crc16 and crc32 checksum routines as alternative to fletcher()
      algorithms

--- src/InoCRC.cpp
+++ src/InoCRC.cpp
@@ -3,23 +3,56 @@
 INO_NAMESPACE
 
 
+/*****************************************************************************/
+ino_u16    crc16(
+  ino_u16 crc,
+  const ino_u8* data_start,
+  const ino_u8* data_end)
+{
+  ino_u8 x;
+
+  crc = ~crc;
+  for ( ; data_start<data_end; ) {
+    x = crc >> 8 ^ *data_start++;
+    x ^= (x >> 4);
+    crc = (crc<<8) ^ ((ino_u16)(x<<12)) ^ ((ino_u16)(x<<5)) ^ ((ino_u16)x);
+  }
+  return ~crc;
+}
+
 ino_u16    crc16_CCITT(
   const ino_u8* data_start,
   const ino_u8* data_end)
 {
-  ino_u16 crc = 0xFFFF;
-  ino_u8 x;
-      
-  while (data_start<data_end)
-  {
-    x  = crc >> 8 ^ *data_start++;
-    x ^= x>>4;
-    crc = (crc<<8) ^ ((ino_u16)(x<<12)) ^ ((ino_u16)(x<<5)) ^ ((ino_u16)x);
-  }
-  return crc;
+  return ~crc16(0x0, data_start, data_end);
 }
 
+/*****************************************************************************/
+ino_u32    crc32(
+  ino_u32 crc,
+  const ino_u8* data_start,
+  const ino_u8* data_end)
+{
+  crc = ~crc;
+  for ( ; data_start<data_end; ) {
+    crc ^= *data_start++;
+    
+    {
+      crc = (crc & 1) ? ((crc >> 1) ^ 0xedb88320) : (crc >> 1);
+      crc = (crc & 1) ? ((crc >> 1) ^ 0xedb88320) : (crc >> 1);
+      crc = (crc & 1) ? ((crc >> 1) ^ 0xedb88320) : (crc >> 1);
+      crc = (crc & 1) ? ((crc >> 1) ^ 0xedb88320) : (crc >> 1);
 
+      crc = (crc & 1) ? ((crc >> 1) ^ 0xedb88320) : (crc >> 1);
+      crc = (crc & 1) ? ((crc >> 1) ^ 0xedb88320) : (crc >> 1);
+      crc = (crc & 1) ? ((crc >> 1) ^ 0xedb88320) : (crc >> 1);
+      crc = (crc & 1) ? ((crc >> 1) ^ 0xedb88320) : (crc >> 1);
+    }
+  }
+  return ~crc;
+}
+
+/*****************************************************************************/
 ino_u16    fletcher16(
   const ino_u8* data_start,
   const ino_u8* data_end)
--- src/InoCRC.h
+++ src/InoCRC.h
@@ -4,10 +4,22 @@
 
 INO_NAMESPACE
 
+/*****************************************************************************/
 ino_u16    crc16_CCITT(
   const ino_u8* data_start,
   const ino_u8* data_end);
 
+ino_u16    crc16(
+  ino_u16 crc,
+  const ino_u8* data_start,
+  const ino_u8* data_end);
+
+ino_u32    crc32(
+  ino_u32 crc,
+  const ino_u8* data_start,
+  const ino_u8* data_end);
+
+/*****************************************************************************/
 ino_u16    fletcher16(
   const ino_u8* data_start,
   const ino_u8* data_end);
